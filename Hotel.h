b68d4fe4e354df4bba4c842aecadc988f1ddcf84
Thibaut34 <lavinoth2@gmail.com>
2019-11-15 13:48:55 +0100

    ajout Hotel.h

---
+++ Hotel.h
@@ -0,0 +1,26 @@
+/*
+	Fichier: Hotel.h
+	Auteur: Thibaut
+	Date: 7/11
+	Matiere: C-C++ Programming / Esirem 3A IT 
+	*/
+#include <string>
+#include <iostream>
+#ifndef _Hotel_h
+#define _Hotel_h 
+class Hotel
+{
+public:
+	Hotel(int id, std::string name, std::string city, int roomlist);
+	int getId() const;
+	int getRoomlist() const;
+	std::string getName() const;
+	std::string getCity() const;
+
+private:
+int m_id;
+int m_roomlist;
+std::string m_name;
+std::string m_city;	
+};
+#endif
